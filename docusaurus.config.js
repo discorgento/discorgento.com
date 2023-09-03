b7a7c134117e767ee7a670f023683bc3299069ef
Jonatan Machado <jonatanaxe@outlook.com>
2023-09-03 20:33:31 -0300

    edit url link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/discorgento/discorgento.com/edit/main/blog/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/discorgento/discorgento.com/edit/main/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
