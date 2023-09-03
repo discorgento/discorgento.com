75a8409d85d3f7f9aa9dc4913680c9dc94f76f7d
Jonatan Machado <jonatanaxe@outlook.com>
2023-09-03 20:23:03 -0300

    defaultMode: dark

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,6 +84,11 @@ const config = {
           },
         ],
       },
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
       footer: {
         style: 'dark',
         links: [
