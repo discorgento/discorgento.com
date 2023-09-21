f2e041c3bdb92d7eff9bdcae4d20bab72a038b0a
Jonatan Machado <jonatanaxe@outlook.com>
2023-09-21 08:48:11 -0300

    tailwind config fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,md,mdx}", "./docs/**/*.{md,mdx}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: ["class", '[data-theme="dark"]'],
+  corePlugins: {
+    preflight: false,
+  },
+  blocklist: ["container"],
 };
