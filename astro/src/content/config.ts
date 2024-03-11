4ff1e5b7b8c274278a09701e130a23d69b10d3c8
CoutoDev <c.svitor@yahoo.com.br>
2024-03-10 23:04:27 -0300

    feat: create docs content type

--- astro/src/content/config.ts
+++ astro/src/content/config.ts
@@ -15,4 +15,18 @@ const blog = defineCollection({
   }),
 })
 
-export const collections = { blog }
+const docs = defineCollection({
+  type: 'content',
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    moduleShortDescription: z.string(),
+    moduleName: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+})
+
+export const collections = { blog, docs }
