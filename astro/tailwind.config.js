f4bba5a9b277cfbbdcbad9adcffdaf33ef1ffa05
CoutoDev <c.svitor@yahoo.com.br>
2024-03-10 23:03:46 -0300

    style: extends colors on tailwind

--- astro/tailwind.config.js
+++ astro/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: 'var(--accent)',
+        'accent-dark': 'var(--accent-dark)',
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 }
