e7b3fd6e85e3d8033644659f085fa9c473fdc511
CoutoDev <c.svitor@yahoo.com.br>
2024-06-29 23:40:46 -0300

    feat: create authors collection and update blog posts

--- astro/src/content/config.ts
+++ astro/src/content/config.ts
@@ -1,16 +1,25 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection, reference, z } from 'astro:content'
 
 const blogContent = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
-    author: z.string(),
-    image: z.string(),
+    authors: z.array(reference('authors')),
     tags: z.array(z.string()),
   }),
 })
 
+
+const authors = defineCollection({
+  type: 'data',
+  schema: z.object({
+    name: z.string(),
+    portfolio: z.string().url(),
+  })
+});
+
 export const collections = {
   blog: blogContent,
+  authors
 }
